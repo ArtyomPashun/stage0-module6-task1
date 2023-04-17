5cfbf643e1568f28ebaab679312410f0377b0325
Artsiom Pashun <a.pashun@syberry.com>
2023-04-17 23:10:17 +0200

    Finished module 6, task 1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,30 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String colour, int numberOfPaws, boolean hasFur) {
+        this.color = colour;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        StringBuilder str = new StringBuilder();
+        str.append("This animal is mostly ").append(color);
+        if (numberOfPaws == 1) {
+            str.append(". It has ").append(numberOfPaws).append(" paw and");
+        } else {
+            str.append(". It has ").append(numberOfPaws).append(" paws and");
+        }
+        if (hasFur) {
+            str.append(" a fur.");
+        } else {
+            str.append(" no fur.");
+        }
+        return str.toString();
+    }
 
 }
